157b2fbcc4f5dcb9de74a3d3ac8ac6a612cc4d57
EstebanGuilarte <102980332+EstebanGuilarte@users.noreply.github.com>
2023-10-07 22:38:23 -0600

    Creación del proyecto "Find Glasses".
    Se crearon tanto la parte web como la api del proyecto "Find Glasses" del curso "Programación Avanzada en Web".

---
+++ ProyectoWeb/ProyectoWeb/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoWeb.Models;
+using System.Diagnostics;
+
+namespace ProyectoWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
