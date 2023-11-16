d8e044146d8038f23ec191a944765fb887d5bf1b
marianasolanoc <140570313+marianasolanoc@users.noreply.github.com>
2023-11-16 01:32:00 -0600

    Se terminó el consultar, registrar, y actualizar de productos. Eliminar está pendiente de encontrarle el fallo

--- ProyectoWeb/ProyectoWeb/Controllers/ProductosController.cs
+++ ProyectoWeb/ProyectoWeb/Controllers/ProductosController.cs
@@ -16,9 +16,98 @@ namespace ProyectoWeb.Controllers
         }
 
         [HttpGet]
+        [FiltroSeguridad]
         public IActionResult ProductosLista()
         {
+            var productos = _productosModel.ConsultarProductos();
+            return View(productos);
+        }
+
+        [HttpGet]
+        [FiltroSeguridad]
+        public IActionResult RegistrarProducto()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [FiltroSeguridad]
+        public IActionResult RegistrarProducto(ProductosEnt entidad)
+        {
+            if (_productosModel.RegistrarProducto(entidad))
+            {
+                return RedirectToAction("ProductosLista");
+            }
+            else
+            {
+                return View(entidad);
+            }
+        }
+
+        [HttpGet]
+        [FiltroSeguridad]
+        public IActionResult DetalleProducto(long id)
+        {
+            var producto = _productosModel.ConsultarProductoPorId(id);
+
+            if (producto != null)
+            {
+                return View(producto);
+            }
+            else
+            {
+                return RedirectToAction("ProductosLista");
+            }
+        }
+
+        [HttpGet]
+        [FiltroSeguridad]
+        public IActionResult ActualizarProducto(long id)
+        {
+            // Obtener el producto por id 
+            var producto = _productosModel.ConsultarProductoPorId(id);
+
+            if (producto != null)
+            {
+                return View(producto);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [FiltroSeguridad]
+        public IActionResult ActualizarProducto(ProductosEnt entidad)
+        {
+            if (_productosModel.ActualizarProducto(entidad))
+            {
+                return RedirectToAction("ProductosLista");
+            }
+            else
+            {
+                return View(entidad);
+            }
+        }
+
+        //Error Pendiente
+        [HttpPost]
+        [FiltroSeguridad]
+        public IActionResult EliminarProducto(ProductosEnt entidad)
+        {
+            var exito = _productosModel.EliminarProducto(entidad.IdProducto);
+
+            if (exito)
+            {
+                TempData["Mensaje"] = $"Se eliminó el producto con ID: {entidad.IdProducto}";
+            }
+            else
+            {
+                TempData["Mensaje"] = $"No se encontró el producto con ID: {entidad.IdProducto}";
+            }
+
+            return RedirectToAction("ProductosLista");
         }
 
 
--- ProyectoWeb/ProyectoWeb/Models/ProductosModel.cs
+++ ProyectoWeb/ProyectoWeb/Models/ProductosModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Net.Http.Headers;
 using ProyectoWeb.Entities;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace ProyectoWeb.Models
 {
@@ -19,6 +21,91 @@ namespace ProyectoWeb.Models
             _HttpContextAccessor = httpContextAccessor;
             _urlApi = _configuration.GetSection("Llaves:urlApi").Value;
         }
+
+
+        public List<ProductosEnt> ConsultarProductos()
+        {
+            string url = _urlApi + "api/Productos/ConsultarProductos";
+            string token = _HttpContextAccessor.HttpContext.Session.GetString("TokenUsuario");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var resp = _httpClient.GetAsync(url).Result;
+
+            if (resp.IsSuccessStatusCode)
+                return resp.Content.ReadFromJsonAsync<List<ProductosEnt>>().Result;
+            else
+                return new List<ProductosEnt>();
+        }
+
+        public bool RegistrarProducto(ProductosEnt entidad)
+        {
+            try
+            {
+                string url = _urlApi + "api/Productos/RegistrarProducto";
+                string token = _HttpContextAccessor.HttpContext.Session.GetString("TokenUsuario");
+
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var resp = _httpClient.PostAsJsonAsync(url, entidad).Result;
+
+                return resp.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public ProductosEnt ConsultarProductoPorId(long id)
+        {
+            string url = $"{_urlApi}api/Productos/ConsultarProductoPorId/{id}";
+            string token = _HttpContextAccessor.HttpContext.Session.GetString("TokenUsuario");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var resp = _httpClient.GetAsync(url).Result;
+
+            if (resp.IsSuccessStatusCode)
+                return resp.Content.ReadFromJsonAsync<ProductosEnt>().Result;
+            else
+                return null;
+        }
+
+        public bool ActualizarProducto(ProductosEnt entidad)
+        {
+            try
+            {
+                string url = _urlApi + "api/Productos/ActualizarProducto";
+                string token = _HttpContextAccessor.HttpContext.Session.GetString("TokenUsuario");
+
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var resp = _httpClient.PutAsJsonAsync(url, entidad).Result;
+
+                return resp.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool EliminarProducto(long idProducto)
+        {
+            try
+            {
+                string url = $"{_urlApi}api/Productos/EliminarProducto/{idProducto}";
+                string token = _HttpContextAccessor.HttpContext.Session.GetString("TokenUsuario");
+
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var resp = _httpClient.DeleteAsync(url).Result;
+
+                return resp.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
 
 
     }
